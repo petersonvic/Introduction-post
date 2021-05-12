dcd55b43914dd5db1d3ec00ccd58d3e2f37fa765
petersonvic <83926959+petersonvic@users.noreply.github.com>
2021-05-12 18:31:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var name = "Victoria";
+var name2 = "Black"
+
+
+
+
+
+var prompter = prompt("Hey what is your name ?");
+var prompter = prompt("What is your favorite color?");
+
+
+
+
+
+var greeting = "Hello, my name is "+name; 
+var greeting2 = "My favorite color is "+name2;
+
+
+document.querySelector(".words").innerHTML = greeting; 
+document.querySelector(".bob").innerHTML = greeting2; 
